b048400dcd95b7d1a1027cf83991688934376643
Carina-Oliveira1 <carina65483@estudante.ifb.edu.br>
2025-02-08 11:02:32 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifb/classes/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifb.classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 07958954107
+ */
+public class Usuario {
+    private String nomeUsuario;
+    private String email;
+    private String senha;
+    private ArrayList<Contato> contato;
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public ArrayList<Contato> getContato() {
+        return contato;
+    }
+
+    public void setContato(ArrayList<Contato> contato) {
+        this.contato = contato;
+    }
+    
+}
